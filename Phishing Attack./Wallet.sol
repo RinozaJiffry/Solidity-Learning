71e64bddcdedd86a4fa685f58e7dd2407d549716
RinozaJiffry <66359174+RinozaJiffry@users.noreply.github.com>
2021-09-23 23:49:35 +0530

    phishing attack

---
+++ Phishing Attack./Wallet.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.10;
+
+contract Wallet {
+    address public owner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    function deposit() public payable {}
+    
+    function transfer(address payable _to, uint _amount) public {
+        require(tx.origin == owner,"Now Owner");
+        _to.transfer(_amount);
+    }
+    
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+}
